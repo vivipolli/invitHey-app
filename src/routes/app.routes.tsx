d5121fd863ee12a707ed8ff60e54956877debb6f
vivipolli <pollivivian@gmail.com>
2021-09-16 12:51:03 -0300

    refactor tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
 import { isEmpty } from 'lodash';
-import { MaterialIcons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import * as Routes from '../pages';
-import { Header } from '../components/Header';
-import { ThemeProvider } from '../../styled-components';
-import { theme } from '../../theme';
+import { Tabs } from './tabs.routes';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -26,79 +23,7 @@ export function Router() {
     )
   } else {
     return (
-      <Tab.Navigator
-        screenOptions={{
-          tabBarInactiveTintColor: '#898D9A',
-          tabBarActiveTintColor: '#FF7527',
-          tabBarShowLabel: false
-        }}
-      >
-        <Tab.Screen
-          name='Onboarding'
-          component={Routes.Onboarding}
-          options={{
-            headerShown: false,
-            tabBarIcon: (({ size, color }) => (
-              <MaterialIcons
-                name='home'
-                size={size}
-                color={color}
-              />
-            ))
-          }}
-        />
-        <Tab.Screen
-          name='SignIn'
-          component={Routes.SignIn}
-          options={{
-            header: props =>
-              <ThemeProvider theme={theme}>
-                <Header page='Meus Eventos' />
-              </ThemeProvider>,
-            tabBarIcon: (({ size, color }) => (
-              <MaterialIcons
-                name='event'
-                size={size}
-                color={color}
-              />
-            ))
-          }}
-        />
-        <Tab.Screen
-          name='SignUp'
-          component={Routes.SignUp}
-          options={{
-            header: props =>
-              <ThemeProvider theme={theme}>
-                <Header page='Criar Evento' />
-              </ThemeProvider>,
-            tabBarIcon: (({ size, color }) => (
-              <MaterialIcons
-                name='add-circle-outline'
-                size={size}
-                color={color}
-              />
-            ))
-          }}
-        />
-        <Tab.Screen
-          name='Welcome'
-          component={Routes.Welcome}
-          options={{
-            header: (props) =>
-              <ThemeProvider theme={theme}>
-                <Header page='Meu Perfil' />
-              </ThemeProvider>,
-            tabBarIcon: (({ size, color }) => (
-              <MaterialIcons
-                name='person-outline'
-                size={size}
-                color={color}
-              />
-            ))
-          }}
-        />
-      </Tab.Navigator>
+      <Tabs />
     )
   }
 }
---
+++ src/routes/tabs.routes.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import * as Routes from '../pages';
+import { Header } from '../components/Header';
+import { ThemeProvider } from '../../styled-components';
+import { theme } from '../../theme';
+
+const Tab = createBottomTabNavigator();
+
+export function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarInactiveTintColor: '#898D9A',
+        tabBarActiveTintColor: '#FF7527',
+        tabBarShowLabel: false
+      }}
+    >
+      <Tab.Screen
+        name='Onboarding'
+        component={Routes.Onboarding}
+        options={{
+          headerShown: false,
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name='home'
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+      <Tab.Screen
+        name='SignIn'
+        component={Routes.SignIn}
+        options={{
+          header: props =>
+            <ThemeProvider theme={theme}>
+              <Header page='Meus Eventos' />
+            </ThemeProvider>,
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name='event'
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+      <Tab.Screen
+        name='SignUp'
+        component={Routes.SignUp}
+        options={{
+          header: props =>
+            <ThemeProvider theme={theme}>
+              <Header page='Criar Evento' />
+            </ThemeProvider>,
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name='add-circle-outline'
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+      <Tab.Screen
+        name='Welcome'
+        component={Routes.Welcome}
+        options={{
+          header: (props) =>
+            <ThemeProvider theme={theme}>
+              <Header page='Meu Perfil' />
+            </ThemeProvider>,
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons
+              name='person-outline'
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
